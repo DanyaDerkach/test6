0d6cc1ca9071bc59f8643af99fe1f50707693962
DanyaDerkach <danyderkach@gmail.com>
2024-11-26 19:34:48 +0200

    edit for lab9

---
+++ LR10_NewFile.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	
+	//коментар
+    float x, a, b, c, f;
+    cout << "Введіть x: ";
+    cin >> x;
+    cout << "Введіть a: ";
+    cin >> a;
+    cout << "Введіть b: ";
+    cin >> b;
+    cout << "Введіть c: ";
+    cin >> c;
+    
+    if (x < 0 && b != 0) {
+        f = (-a * pow(x, 2)) + b;
+    } 
+    else if (x > 0 && b == 0) {
+        f = (x / (x - c)) + 5.5;
+    } 
+    else {
+        f = x / (-c);
+    }
+    
+    cout << "F = " << f << endl;
+
+    return 0;
+}
