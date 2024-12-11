1e966d0393997633112605e24250a556b1a21eea
DanyaDerkach <danyderkach@gmail.com>
2024-12-11 05:51:34 +0200

    edit LR12_Derkach_109.cpp

--- LR12_Derkach_109.cpp
+++ LR12_Derkach_109.cpp
@@ -1,36 +1,14 @@
 #include <iostream>
-#include <cmath> // Для математичних операцій
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    float number, a, b ,c;
-    cout << "Введіть ";
-	cout << "Введіть двозначне ";
-	cout << "Введіть двозначне число ";
-	cout << "Введіть двозначне число типу float: ";
-    cin >> number;
-
-    // Переведення числа до цілого типу через static_cast
-    int int_number = static_cast<int>(number);
-
-    // Перевірка, чи є число двозначним
-    if (int_number < 10 || int_number > 99) {
-        cout << "Помилка: введене число не є двозначним." << endl;
-        return 1;
-    }
-
-    // Отримання цифр числа
-    int first_digit = int_number / 10;
-    int second_digit = int_number % 10;
-
-    // Сума та добуток цифр
-    int sum = first_digit + second_digit;
-    int product = first_digit * second_digit;
-
-    // Виведення результатів
-    cout << "Сума цифр: " << sum << endl;
-    cout << "Добуток цифр: " << product << endl;
-
+    cout << "Добуток цифр: " << endl;
+	cout << "Добуток цифр: " << endl;
+	cout << "Добуток цифр: " << endl;
+	cout << "Добуток цифр: " << endl;
+	cout << "Добуток цифр: " << endl;
+	cout << "Добуток цифр: " << endl;
     return 0;
 }
